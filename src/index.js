cc793fa88912d4b108ae389d8f7d682edf44e094
amirbehesti <amirbehesti392@gmail.com>
2022-09-15 19:24:49 +0530

    fix7

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const myFileWriter = async (fileName, fileContent) => {
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
-	await fs.readFile(fileName, 'utf8', (err, data) => {
+	await fs.readFile(fileName,(err, data) => {
 		if (err) {
 		  console.error(err);
 		  return;
