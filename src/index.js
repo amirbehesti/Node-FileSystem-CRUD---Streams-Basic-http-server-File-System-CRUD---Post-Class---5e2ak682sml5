04058432be818d8ff28202524521c3bd49463bb3
amirbehesti <amirbehesti392@gmail.com>
2022-09-15 18:50:12 +0530

    first-fix

--- src/index.js
+++ src/index.js
@@ -3,22 +3,49 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+ await fs.appendFile(fileName, fileContent, function (err) {
+  if (err){
+	console.error(err);
+	return;
+  };
+  console.log('Saved!');
+});
 }
-
+myFileWriter("input.txt", "Hello");
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	await fs.readFile(fileName, 'utf8', function(err, data){
+		if (err) {
+		  console.error(err);
+		  return;
+		}
+		console.log(data);
+	  });
 }
-
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	await fs.appendFile(fileName, fileContent, function (err) {
+		if (err){
+			console.error(err);
+			return;
+		}
+		console.log('Updated!');
+	  });
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	await fs.unlink(fileName, function (err) {
+		if (err){
+			console.error(err);
+			return;
+		}
+		console.log('File deleted!');
+	  });
 }
 
 
